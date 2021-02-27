ad26281a516d2a1ebf2549116d295baf1b398f61
PauloAlves8039 <pj38alves@gmail.com>
2021-02-27 15:34:23 -0300

    Adicionando e implementando o enum TipoConta

--- src/DIO.Bank/Classes/Conta.cs
+++ src/DIO.Bank/Classes/Conta.cs
@@ -1,7 +1,9 @@
+using src.DIO.Bank.Enum;
+
 namespace src.DIO.Bank.Classes
 {
     public class Conta
     {
-
+        private TipoConta TipoConta { get; set; }
     }
 }
---
+++ src/DIO.Bank/Enum/TipoConta.cs
@@ -0,0 +1,8 @@
+namespace src.DIO.Bank.Enum
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
